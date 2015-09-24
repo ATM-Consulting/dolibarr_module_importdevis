d9933161099be648ac15ca0bbf42a53148ccc69f
Alexis Algoud <alexis@atm-consulting.fr>
2015-09-24 16:05:58 +0200

    Parsing et preview import

--- importdevis.php
+++ importdevis.php
@@ -24,12 +24,81 @@
 	
 	if ($action == 'send_dgpf')
 	{
-		importFile($db, $conf, $langs);
-		header('Location: '.dol_buildpath('/importdevis/importdevis.php?id='.$id, 2));
+		$TData = importFile($db, $conf, $langs);
+		fiche_preview($object, $TData);
+		
+		exit;
+	}
+	else {
+		fiche_import($object);
+	}
+	
+function fiche_preview(&$object, &$TData) {
+	
+	global $langs, $user;
+
+    $head = propal_prepare_head($object);
+
+	if (empty($user->rights->importdevis->read))
+	{
+		accessforbidden();
 		exit;
 	}
 	
+		llxHeader();
+		$title = $langs->trans('Import');
+	    dol_fiche_head($head, 'importdevis', $title, 0, 'propal');
+		
+		?><table width="100%" class="border">
+			<tr>
+				<td width="25%"><?php echo $langs->trans('Ref'); ?></td>
+				<td colspan="3"><div style="vertical-align: middle"><div class="inline-block floatleft refid"><?php echo $object->ref; ?></div></div></td>
+			</tr>
+			<tr>
+				<td><?php echo $langs->trans('Company'); ?></td>
+				<td colspan="3"><?php echo $object->thirdparty->getNomUrl(1); ?></td>
+			</tr>
+			<tr>
+				<td colspan="4">
+					<form name="to_parse" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+						<input name="action" type="hidden" value="save_data" />
+						<input name="token" type="hidden" value="<?php echo $_SESSION['newtoken']; ?>" />
+						<input name="data" type="hidden" value="<?php echo base64_encode(serialize($TData)); ?>" />
+						<table class="border" width="100%">
+						<?php
+						
+							foreach($TData as &$row) {
+									
+								if($row['type'] == 'title') {
+									print '<tr class="liste_titre">';	
+								}	
+								
+								print '<td>'.$row['label'].'</td>';
+								print '<td>'.$row['qty'].'</td>';
+								
+								print '</tr>';
+								
+							}
+									
+														
+						
+						?>
+						</table>
+						<input class="button" type="submit" value="<?php echo $langs->trans('Save'); ?>" />
+					</form>
+				</td>
+			</tr>
+		</table>
+	<?php
+	    
+	    dol_fiche_end();
+		llxFooter();	
+		
 	
+}
+function fiche_import(&$object) {
+	global $langs, $user;
+
     $head = propal_prepare_head($object);
 
 	if (empty($user->rights->importdevis->read))
@@ -39,7 +108,7 @@
 	else 
 	{
 		llxHeader();
-		$title = $langs->trans('importDGPF');
+		$title = $langs->trans('Import');
 	    dol_fiche_head($head, 'importdevis', $title, 0, 'propal');
 		
 		?>
@@ -58,7 +127,7 @@
 				<td>
 					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
 						<input name="action" type="hidden" value="send_dgpf" />
-						<input name="id" type="hidden" value="<?php echo $id; ?>" />
+						<input name="id" type="hidden" value="<?php echo $object->id; ?>" />
 						<input name="token" type="hidden" value="<?php echo $_SESSION['newtoken']; ?>" />
 						
 						<input name="fileDGPF" type="file" />
@@ -75,4 +144,4 @@
 	    dol_fiche_end();
 		llxFooter();
 	}
-    
+}    
--- lib/importdevis.lib.php
+++ lib/importdevis.lib.php
@@ -53,15 +53,39 @@ function importdevisAdminPrepareHead()
 
     return $head;
 }
-function _importFileParseCSV(&$file)  {
+function _importFileParseCSV(&$file, $nb_line_to_avoid)  {
+global $conf;
+
 	$handle = fopen($file['tmp_name'], 'r');
+	$method = 'lineMapper_'.$conf->global->IMPORTPROPAL_FORMAT;
+	$method_after = 'dataParserAfter_'.$conf->global->IMPORTPROPAL_FORMAT;
 	
+	$k = 0;
 	$TData = array();
 	while ($line = fgetcsv($handle, 4096, ';'))
 	{
+		if($k<$nb_line_to_avoid) {
+			null;
+		}
+		else{
+			if(function_exists($method)) {
+				$line = call_user_func($method, $line);
+		    } 
+			
+			if (!empty($line) ) {
+				$TData[] = $line;
+		  	}
+			
+		}
 		
-		$TData[] = $line;
+		 
+		 $k++;
 	}
+	
+	if(function_exists($method_after)) {
+			$TData = call_user_func($method_after, $TData);
+	 } 
+	
 	
 	fclose($handle);
 	
@@ -78,7 +102,14 @@ function lineMapper_DGPF(&$line) {
 	
 	if (empty($line[1]) && empty($line[2]) && empty($line[4]) && empty($line[6]) && empty($line[8]) && empty($line[9])) return false;
 	
-	return $line;
+	return $line;			if(function_exists($method)) {
+				$line = call_user_func($method, $line);
+		    } 
+			
+			if (!empty($line) ) {
+				$TData[] = $line;
+		  	}
+	
 }
 
 function lineMapper_SMARTBOM($line) {
@@ -87,12 +118,77 @@ function lineMapper_SMARTBOM($line) {
 	if(empty($line[11])) return false;
 	
 	$Tab=array(
-		'label'=>$line[11]
+		'label'=>empty($line[4]) ? $line[11] : $line[4] 
 		,'qty'=>empty($line[14]) ? 1 : (float)$line[14]
+		,'type'=>'line'
+		,'product_ref'=>$line[5]
+		,'title1'=>$line[1]
+		,'title2'=>$line[2]
+		,'title3'=>$line[3]
+		,'level'=>0
 	);
 	
 	return $Tab;
 	
+}
+
+function _dPA_SMARTBOM_add_title(&$Tab, $label, $level=1) {
+	
+	$nb = count($Tab);
+	
+	$found = false;
+	for($k = $nb-1;$k>=0;$k--) {
+		
+		if($Tab[$k]['type'] == 'title' && $Tab[$k]['label'] === $label && $Tab[$k]['level'] == $level) {
+			$found = true;
+			break;
+		}
+		else if($Tab[$k]['type'] == 'title' && $Tab[$k]['level'] == $level) {
+			break;
+		}
+		
+	}
+	
+	if(!$found) {
+		
+		/*$Tab[]=array(
+			'label'=>'Sous-total'
+			,'type'=>'subtotal'
+			,'qty'=>0
+			,'level'=>$level
+			
+		)*/
+		
+		$Tab[] = array(
+			'label'=>$label
+			,'qty'=>0
+			,'level'=>$level
+			,'type'=>'title'
+		
+		);
+		
+	}
+	
+	
+	
+	if($found === false) return false;
+	else return $k;
+}
+
+function dataParserAfter_SMARTBOM($TData) {
+	
+	$Tab = array();
+	
+	foreach($TData as &$row) {
+		
+		_dPA_SMARTBOM_add_title($Tab, $row['title1']);
+		_dPA_SMARTBOM_add_title($Tab, $row['title2'],2);
+		_dPA_SMARTBOM_add_title($Tab, $row['title3'],3);
+		
+		$Tab[] = $row;
+	}
+
+	return $Tab;
 }
 
 function _importFileParseXLS2(&$file, $nb_line_to_avoid) {
@@ -101,6 +197,7 @@ function _importFileParseXLS2(&$file, $nb_line_to_avoid) {
 	set_time_limit(0);
 	$TData=array();
 	$method = 'lineMapper_'.$conf->global->IMPORTPROPAL_FORMAT;
+	$method_after = 'dataParserAfter_'.$conf->global->IMPORTPROPAL_FORMAT;
 	
 	$filename = sys_get_temp_dir().'/'. $file['name'];
 	copy($file['tmp_name'], $filename);
@@ -137,8 +234,18 @@ function _importFileParseXLS2(&$file, $nb_line_to_avoid) {
 	  }
 	  
 	  
-	}
+	}			if(function_exists($method)) {
+				$line = call_user_func($method, $line);
+		    } 
+			
+			if (!empty($line) ) {
+				$TData[] = $line;
+		  	}
 	
+	
+	if(function_exists($method_after)) {
+			$TData = call_user_func($method_after, $TData);
+	  } 
 	
 	return $TData;
 	
@@ -151,6 +258,7 @@ global $conf;
 	dol_include_once('/importdevis/lib/spreadsheet-reader/SpreadsheetReader.php');
 
 	$method = 'lineMapper_'.$conf->global->IMPORTPROPAL_FORMAT;
+	$method_after = 'dataParserAfter_'.$conf->global->IMPORTPROPAL_FORMAT;
 	
 	$filename = sys_get_temp_dir().'/'. $file['name'];
 	copy($file['tmp_name'], $filename);
@@ -175,6 +283,11 @@ global $conf;
 				
 	}
 	
+	if(function_exists($method_after)) {
+			$TData = call_user_func($method_after, $TData);
+	 } 
+	
+	
 	return $TData;
 }
 
@@ -191,45 +304,14 @@ function importFile(&$db, &$conf, &$langs)
 	else {
 		
 	}
-	/*
-	if($file['type'] == 'text/csv') {
-		$TData = _importFileParseCSV($file);
+	if(strtolower($info['extension']) == 'csv') {
+		$TData = _importFileParseCSV($file, GETPOST('nb_line_to_avoid'));
 	}
-	else {*/
-		$TData = _importFileParseXLS2($file, GETPOST('nb_line_to_avoid'));
-	//}
-	/*
-	 * [0] => ''
-	 * [1] => Indice de grand titre
-	 * [2] => Indice sous titre (ex : 3.2.1.2 = indice de niveau 4)
-	 * [3] => ''
-	 * [4] => Description
-	 * [5] => ''
-	 * [6] => Sous-total et sous-sous-total
-	 * [7] => ''
-	 * [8] => Unité
-	 * [9] => Qté
-	 * [10]=> ''
-	 * [11]=> ''
-	 * 
-	 */
-	 
-	 
-	
-	
-	
-	
-	/*
-	 * Règles : 
-	 *  - Si unité => ligne d’ouvrage, reliée à la 1ère ligne de titre du dessus
-	 *  - Si description uniquement => description de l’ouvrage
-	 *  - Si quantité vide => Mettre 999999 (voir affichage en rouge)
-	 *  - Si indice et pas de quantité ni d’unité => ligne de chapitre
-	 */
-	foreach ($TData as $line)
-	{		
-		var_dump($line);
+	else {
+		$TData = _importFileParseXLS2($file, GETPOST('nb_line_to_avoid'));	
 	}
 	
-	exit;
+	
+	return $TData;
+	
 }
